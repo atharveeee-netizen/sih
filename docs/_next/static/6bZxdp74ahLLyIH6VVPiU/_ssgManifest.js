ddb92bedead07400a648718d88e1a462e5fbeeb4
Atharve Dahima <atharveeee@gmail.com>
2026-08-29 18:28:24 +0530

    feat(pages): publish static site directly to /docs directory with .nojekyll for instant GitHub Pages hosting

---
+++ docs/_next/static/6bZxdp74ahLLyIH6VVPiU/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fverify\u002F[batchId]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ docs/_next/static/chunks/3dthz_hoesqg5.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,1139,e=>{e.v(o=>Promise.all(["static/chunks/3vafpi6eujuax.js"].map(o=>e.l(o))).then(()=>o(9963)))},89892,e=>{e.v(e=>Promise.resolve().then(()=>e(37575)))}]);
